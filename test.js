9ddae2f89b9e7a94e69e752eeb63e2f8f4e807d6
jasonzuo <1184881898@qq.com>
2025-06-02 20:24:16 +0800

    remove unused file

--- test.js
+++
@@ -1,6 +0,0 @@
-function validateDate(date ,format) {
-    const regex = new RegExp(`^\\d{4}${format}\\d{2}${format}\\d{2}$`);
-    return regex.test(date) && !isNaN(new Date(date).getTime());
-}
-
-console.log(validateDate('2023/10/05', '/')); // true
